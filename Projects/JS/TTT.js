4e44e1b7ef0775586470e44e5e496383264b2550
Madhavan-Maddox <madhavanmaddox289@gmail.com>
2024-07-16 16:05:03 +0530

    add changes

---
+++ Projects/JS/TTT.js
@@ -0,0 +1,8 @@
+let btn = document.querySelectorAll(".box");
+let reset = document.querySelector(".b2");
+
+let turnO = true;
+
+btn.addEventListener("click", (e) =>{
+    console.log("Btn clicked");
+});
